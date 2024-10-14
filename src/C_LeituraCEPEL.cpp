13551c82252df559fb80f4e9670a37d0fac9316e
Felipe Beltrán <felipebeltranrodriguez@gmail.com>
2024-10-14 17:44:00 -0300

    ajustes PARCIAIS, falta ajustar a info Termelétrica para o cálculo dos cortes NW

--- src/C_LeituraCEPEL.cpp
+++ src/C_LeituraCEPEL.cpp
@@ -4439,22 +4439,45 @@ void LeituraCEPEL::leitura_cortes_NEWAVE(Dados& a_dados, const SmartEnupla<Perio
 
 				if (a_dados.getAtributo(idUTE, AttComumTermeletrica_lag_mensal_potencia_disponivel_comandada, int()) > 0) {
 
+					const IdUnidadeUTE idUnidadeUTEIni = a_dados.vetorTermeletrica.at(idUTE).getMenorId(IdUnidadeUTE());
+					const IdUnidadeUTE idUnidadeUTEOut = a_dados.vetorTermeletrica.at(idUTE).getIdOut(IdUnidadeUTE());
+
 					const IdSubmercado idSubmercado = a_dados.getAtributo(idUTE, AttComumTermeletrica_submercado, IdSubmercado());
 
 					estados_GNL.at(idUTE) = SmartEnupla<IdSubmercado, SmartEnupla<int, IdVariavelEstado>>(idSubmercado, std::vector<SmartEnupla<int, IdVariavelEstado>>(1, SmartEnupla<int, IdVariavelEstado>(1, std::vector<IdVariavelEstado>(lag_GNL, IdVariavelEstado_Nenhum))));
+				
+					double potencia_minima = 0.0;
+					double potencia_maxima = getdoubleFromChar("max");
+					//double potencia_maxima = 0.0;
 
-					double potencia_disponivel_maxima = 0.0;
-					double potencia_disponivel_minima = 0.0;
+					/*
 					for (IdPatamarCarga idPat = IdPatamarCarga_1; idPat <= a_dados.getIterador2Final(AttMatrizDados_percentual_duracao_patamar_carga, a_horizonte_estudo.getIteradorFinal(), IdPatamarCarga()); idPat++) {
 						const double percentual_duracao = a_dados.getElementoMatriz(AttMatrizDados_percentual_duracao_patamar_carga, a_horizonte_estudo.getIteradorFinal(), idPat, double());
-						potencia_disponivel_minima = a_dados.getElementoMatriz(idUTE, AttMatrizTermeletrica_potencia_disponivel_minima, a_horizonte_estudo.getIteradorFinal(), idPat, double()) * percentual_duracao;
-						potencia_disponivel_maxima = a_dados.getElementoMatriz(idUTE, AttMatrizTermeletrica_potencia_disponivel_maxima, a_horizonte_estudo.getIteradorFinal(), idPat, double()) * percentual_duracao;
-					}
+						
+						//potencia_minima
+						if (a_dados.getSizeMatriz(idUTE, AttMatrizTermeletrica_potencia_minima) > 0 && a_dados.getSizeMatriz(idUTE, AttMatrizTermeletrica_potencia_util) > 0) {
+							potencia_minima = a_dados.getElementoMatriz(idUTE, AttMatrizTermeletrica_potencia_minima, a_horizonte_estudo.getIteradorFinal(), idPat, double()) * percentual_duracao;
+							potencia_maxima = potencia_minima + a_dados.getElementoMatriz(idUTE, AttMatrizTermeletrica_potencia_util, a_horizonte_estudo.getIteradorFinal(), idPat, double()) * percentual_duracao;
+						}//if (a_dados.getSizeMatriz(idUTE, AttMatrizTermeletrica_potencia_minima) > 0 && a_dados.getSizeMatriz(idUTE, AttMatrizTermeletrica_potencia_util) > 0) {
+						else if (a_dados.vetorTermeletrica.at(idUTE).getSizeMatriz(idUnidadeUTEIni, AttMatrizUnidadeUTE_potencia_minima) > 0 && a_dados.vetorTermeletrica.at(idUTE).getSizeMatriz(idUnidadeUTEIni, AttMatrizUnidadeUTE_potencia_util) > 0) {
 
-					estados_GNL.at(idUTE).at(idSubmercado).at(1) = estagio_pos_estudo.addVariavelEstado(TipoSubproblemaSolver_geral, std::string(strVarDecisaoPTDISPCOMIdEstagio + "," + getString(periodo_pos_estudo + 1) + "," + getString(idUTE) + "," + getString(potencia_disponivel_minima) + "," + getString(potencia_disponivel_maxima)), -1, -1);
+							for (IdUnidadeUTE idUnidade = idUnidadeUTEIni; idUnidade < idUnidadeUTEOut; a_dados.vetorTermeletrica.at(idUTE).vetorUnidadeUTE.incr(idUnidade)) {
+								potencia_minima += a_dados.vetorTermeletrica.at(idUTE).getElementoMatriz(idUnidadeUTEIni, AttMatrizUnidadeUTE_potencia_minima, a_horizonte_estudo.getIteradorFinal(), idPat, double()) * percentual_duracao;
+								potencia_maxima += (a_dados.vetorTermeletrica.at(idUTE).getElementoMatriz(idUnidadeUTEIni, AttMatrizUnidadeUTE_potencia_minima, a_horizonte_estudo.getIteradorFinal(), idPat, double()) + a_dados.vetorTermeletrica.at(idUTE).getElementoMatriz(idUnidadeUTEIni, AttMatrizUnidadeUTE_potencia_util, a_horizonte_estudo.getIteradorFinal(), idPat, double())) * percentual_duracao;
+							
+							}//for (IdUnidadeUTE idUnidade = idUnidadeUTEIni; idUnidade < idUnidadeUTEOut; a_dados.vetorTermeletrica.at(idUTE).vetorUnidadeUTE.incr(idUnidade)) {
+
+						}//else if (a_dados.vetorTermeletrica.at(idUTE).getSizeMatriz(idUnidadeUTEIni, AttMatrizUnidadeUTE_potencia_minima) > 0 && a_dados.vetorTermeletrica.at(idUTE).getSizeMatriz(idUnidadeUTEIni, AttMatrizUnidadeUTE_potencia_util) > 0) {
+						else { throw std::invalid_argument("Deve ser informado: AttMatrizTermeletrica_potencia_minima e AttMatrizTermeletrica_potencia_util ou AttMatrizUnidadeUTE_potencia_minima e  AttMatrizUnidadeUTE_potencia_minima_potencia_util na termeletrica: " + getString(idUTE)); }
+											
+
+					}
+					*/
+
+					estados_GNL.at(idUTE).at(idSubmercado).at(1) = estagio_pos_estudo.addVariavelEstado(TipoSubproblemaSolver_geral, std::string(strVarDecisaoPTDISPCOMIdEstagio + "," + getString(periodo_pos_estudo + 1) + "," + getString(idUTE) + "," + getString(potencia_minima) + "," + getString(potencia_maxima)), -1, -1);
 					estados.addElemento(estados_GNL.at(idUTE).at(idSubmercado).at(1), 0.0);
 
-					estados_GNL.at(idUTE).at(idSubmercado).at(2) = estagio_pos_estudo.addVariavelEstado(TipoSubproblemaSolver_geral, std::string(strVarDecisaoPTDISPCOMIdEstagio + "," + getString(periodo_pos_estudo) + "," + getString(idUTE) + "," + getString(potencia_disponivel_minima) + "," + getString(potencia_disponivel_maxima)), -1, -1);
+					estados_GNL.at(idUTE).at(idSubmercado).at(2) = estagio_pos_estudo.addVariavelEstado(TipoSubproblemaSolver_geral, std::string(strVarDecisaoPTDISPCOMIdEstagio + "," + getString(periodo_pos_estudo) + "," + getString(idUTE) + "," + getString(potencia_minima) + "," + getString(potencia_maxima)), -1, -1);
 					estados.addElemento(estados_GNL.at(idUTE).at(idSubmercado).at(2), 0.0);
 
 				}
